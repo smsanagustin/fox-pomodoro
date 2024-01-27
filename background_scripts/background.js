82b54cb675e87ea67e4b979c9ae6f5e99b449245
Sophia San Agustin <smsanagustin@up.edu.ph>
2024-01-28 03:02:55 +0800

    feat: added start/pause feature

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -1,19 +1,33 @@
 let timer;
+let currentTime = 60;
+let timerRunning = false;
 
-function startTimer(duration) {
-  let remaining = duration;
-  browser.browserAction.setBadgeText({ text: String(remaining) });
+function startTimer() {
+  timerRunning = true;
+
+  browser.browserAction.setBadgeText({ text: String(currentTime) });
   clearInterval(timer);
+
   timer = setInterval(() => {
-    remaining--;
-    browser.browserAction.setBadgeText({ text: String(remaining) });
+    currentTime--;
+    browser.browserAction.setBadgeText({ text: String(currentTime) });
     if (remaining <= 0) {
       clearInterval(timer);
     }
-  }, 60000); // Update every minute
+  }, 60000);
+}
+
+function pauseTimer() {
+  clearInterval(timer);
+  browser.browserAction.setBadgeText({ text: "-" });
+  timerRunning = false;
 }
 
 browser.browserAction.onClicked.addListener(() => {
-  startTimer(60); // Start a 60-minute timer
+  if (!timerRunning) {
+    startTimer(); // start timer for 60 minutes
+  } else {
+    pauseTimer();
+  }
 });
 
